19edfca343cdf1ca88e188c30382e431aff35b31
higeojisan <makotokoda82@gmail.com>
2019-04-15 21:03:22 +0900

    定型文を返信

--- handler.rb
+++ handler.rb
@@ -14,6 +14,16 @@ def hello(event:, context:)
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
 
-  response = client.reply_message("<replyToken>", message)
-  p response
+  body = event["body"]
+  requests = client.parse_events_from(body)
+  requests.each do |req|
+    case req
+    when Line::Bot::Event::Message
+      case req.type
+      when Line::Bot::Event::MessageType::Text
+        response = client.reply_message(req['replyToken'], message)
+        p response
+      end
+    end
+  end
 end
