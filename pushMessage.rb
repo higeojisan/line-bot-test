dc5dd4b460158a29512dcdc163739a95da7ef0df
higeojisan <makotokoda82@gmail.com>
2019-04-18 19:16:55 +0900

    明日の天気のプッシュメッセージを作成

--- pushMessage.rb
+++ pushMessage.rb
@@ -2,6 +2,29 @@ load "vendor/bundle/bundler/setup.rb"
 
 require 'json'
 require 'line/bot'
+require 'httpclient'
+require 'jsonclient'
+
+def getWeatherInfo()
+  client = JSONClient.new
+  res = client.get("http://weather.livedoor.com/forecast/webservice/json/v1?city=130010")
+  status = res.status
+  body = res.body.to_h
+  forecasts = body['forecasts']
+  result = "明日の天気\n\n"
+  link = body['link']
+  forecasts.each do |forecast|
+    if forecast['dateLabel'] == '明日'
+      result += forecast['telop'] + "\n"
+      max_temp = forecast['temperature']['max']['celsius']
+      min_temp = forecast['temperature']['min']['celsius']
+      result += "最高気温: #{max_temp}" + "\n"
+      result += "最低気温: #{min_temp}" + "\n"
+      result += "\n" + link
+    end
+  end
+  result
+end
 
 def weather(event:, context:)
   client = Line::Bot::Client.new { |config|
@@ -9,9 +32,11 @@ def weather(event:, context:)
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
   }
 
+  weatherInfo = getWeatherInfo()
+
   message = {
     type: 'text',
-    text: 'push message'
+    text: "#{weatherInfo}"
   }
   client.push_message(ENV["MY_LINE_USER_ID"], message)
 end
